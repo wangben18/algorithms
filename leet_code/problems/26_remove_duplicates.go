c81fba7de737b1cf02a3225bae8b106a9196784b
Wang Ben <wangben18@outlook.com>
2022-10-01 18:21:07 +0800

    add leetcode problem: 26 remove duplicates

---
+++ leet_code/problems/26_remove_duplicates.go
@@ -0,0 +1,12 @@
+package problems
+
+func removeDuplicates(nums []int) int {
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
