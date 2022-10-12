a3c50128605cd2fedc8492484ba80616b5e9ca64
Wang Ben <wangben18@outlook.com>
2022-10-12 19:57:41 +0800

    20_valid_parentheses

---
+++ leet_code/problems/20_valid_parentheses.go
@@ -0,0 +1,26 @@
+package problems
+
+func isValid(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	if len(s)%2 != 0 {
+		return false
+	}
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+			continue
+		}
+		if len(stack) == 0 {
+			return false
+		}
+		if (s[i] == ')' && stack[len(stack)-1] == '(') || (s[i] == ']' && stack[len(stack)-1] == '[') || (s[i] == '}' && stack[len(stack)-1] == '{') {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
---
+++ leet_code/problems/20_valid_parentheses_test.go
@@ -0,0 +1,29 @@
+package problems
+
+import "testing"
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{
+				s: "()",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.s); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
