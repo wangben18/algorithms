dc7b13d451b7371f75dc1789d3f62db623bd0450
Wang Ben <wangben18@outlook.com>
2022-10-23 16:50:04 +0800

    513_find_bottom_left_tree_value

---
+++ leet_code/problems/513_find_bottom_left_tree_value.go
@@ -0,0 +1,36 @@
+package problems
+
+import "container/list"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findBottomLeftValue(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := list.New()
+	queue.PushBack(root)
+	result := 0
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if i == 0 {
+				result = node.Val
+			}
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+	}
+	return result
+}
