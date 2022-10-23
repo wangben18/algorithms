2e397ec16578cb7fee360090571c50593d238dcc
Wang Ben <wangben18@outlook.com>
2022-10-23 16:03:35 +0800

    404_sum_of_left_leaves

---
+++ leet_code/problems/404_sum_of_left_leaves.go
@@ -0,0 +1,21 @@
+package problems
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftSum := sumOfLeftLeaves(root.Left)
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		leftSum = root.Left.Val
+	}
+	rightSum := sumOfLeftLeaves(root.Right)
+	return leftSum + rightSum
+}
