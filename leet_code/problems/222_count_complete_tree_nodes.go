f3a1cf62a1de72599964c1162d12c1a9e068e8f6
Wang Ben <wangben18@outlook.com>
2022-10-23 15:18:02 +0800

    222_count_complete_tree_nodes

---
+++ leet_code/problems/222_count_complete_tree_nodes.go
@@ -0,0 +1,34 @@
+package problems
+
+import "container/list"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	count := 0
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		count += length
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+	}
+	return count
+}
