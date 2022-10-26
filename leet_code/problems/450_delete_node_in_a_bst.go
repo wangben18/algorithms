637cfd81b0b8860c5ae738e01e3e353b90d0b8ac
Wang Ben <wangben18@outlook.com>
2022-10-26 22:13:00 +0800

    450_delete_node_in_a_bst

---
+++ leet_code/problems/450_delete_node_in_a_bst.go
@@ -0,0 +1,37 @@
+package problems
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		if root.Left == nil && root.Right == nil {
+			return nil
+		} else if root.Left != nil && root.Right == nil {
+			return root.Left
+		} else if root.Left == nil && root.Right != nil {
+			return root.Right
+		} else {
+			cur := root.Right
+			for cur.Left != nil {
+				cur = cur.Left
+			}
+			cur.Left = root.Left
+			return root.Right
+		}
+	}
+	if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	}
+	return root
+}
