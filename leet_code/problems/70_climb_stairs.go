03c1397e991bcf97a77e8336bfa47e446dafd612
Wang Ben <wangben18@outlook.com>
2022-11-03 23:16:41 +0800

    70_climb_stairs

--- leet_code/problems/70_climb_stairs.go
+++ leet_code/problems/70_climb_stairs.go
@@ -1,19 +1,34 @@
 package problems
 
+// DP
 func climbStairs(n int) int {
-	return climbStairsWithMemo(n, make(map[int]int))
+	if n <= 2 {
+		return n
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n-1]
 }
 
-func climbStairsWithMemo(n int, memo map[int]int) int {
-	if n == 1 {
-		return 1
-	} else if n == 2 {
-		return 2
-	}
-	if memoNum, ok := memo[n]; ok {
-		return memoNum
-	}
-	num := climbStairsWithMemo(n-1, memo) + climbStairsWithMemo(n-2, memo)
-	memo[n] = num
-	return num
-}
+// 递归（带 memo）
+// func climbStairs(n int) int {
+// 	return climbStairsWithMemo(n, make(map[int]int))
+// }
+
+// func climbStairsWithMemo(n int, memo map[int]int) int {
+// 	if n == 1 {
+// 		return 1
+// 	} else if n == 2 {
+// 		return 2
+// 	}
+// 	if memoNum, ok := memo[n]; ok {
+// 		return memoNum
+// 	}
+// 	num := climbStairsWithMemo(n-1, memo) + climbStairsWithMemo(n-2, memo)
+// 	memo[n] = num
+// 	return num
+// }
