cf9cfda5436989ea18612a7b5f5747b1689010fa
Wang Ben <wangben18@outlook.com>
2022-11-11 16:41:03 +0800

    377_combination_sum_iv

---
+++ leet_code/problems/377_combination_sum_iv.go
@@ -0,0 +1,15 @@
+package problems
+
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for i := 1; i <= target; i++ {
+		for j := 0; j < len(nums); j++ {
+			if i < nums[j] {
+				continue
+			}
+			dp[i] += dp[i-nums[j]]
+		}
+	}
+	return dp[target]
+}
