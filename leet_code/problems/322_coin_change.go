466b51215e77c3c01049b6d63843454b830c3c84
Wang Ben <wangben18@outlook.com>
2023-01-06 16:18:09 +0800

    use MaxInt

--- leet_code/problems/322_coin_change.go
+++ leet_code/problems/322_coin_change.go
@@ -5,16 +5,16 @@ import "math"
 func coinChange(coins []int, amount int) int {
 	dp := make([]int, amount+1)
 	for i := 1; i < len(dp); i++ {
-		dp[i] = math.MaxInt32
+		dp[i] = math.MaxInt
 	}
 	for i := 0; i < len(coins); i++ {
 		for j := coins[i]; j <= amount; j++ {
-			if dp[j-coins[i]] != math.MaxInt32 {
+			if dp[j-coins[i]] != math.MaxInt {
 				dp[j] = min(dp[j-coins[i]]+1, dp[j])
 			}
 		}
 	}
-	if dp[amount] == math.MaxInt32 {
+	if dp[amount] == math.MaxInt {
 		return -1
 	}
 	return dp[amount]
