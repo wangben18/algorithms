2ccd59d74decfd232165c114fb3cb82232cc5679
Wang Ben <wangben18@outlook.com>
2023-01-06 16:04:57 +0800

    322_coin_change

---
+++ leet_code/problems/322_coin_change.go
@@ -0,0 +1,28 @@
+package problems
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = math.MaxInt32
+	}
+	for i := 0; i < len(coins); i++ {
+		for j := coins[i]; j <= amount; j++ {
+			if dp[j-coins[i]] != math.MaxInt32 {
+				dp[j] = min(dp[j-coins[i]]+1, dp[j])
+			}
+		}
+	}
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
