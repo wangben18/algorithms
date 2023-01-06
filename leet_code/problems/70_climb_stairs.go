de8daf8c9783cad8d891357592d6b17c509d8cfe
Wang Ben <wangben18@outlook.com>
2023-01-06 15:14:55 +0800

    70_climb_stairs（完全背包）

--- leet_code/problems/70_climb_stairs.go
+++ leet_code/problems/70_climb_stairs.go
@@ -1,18 +1,32 @@
 package problems
 
-// DP
+// 完全背包
 func climbStairs(n int) int {
-	if n <= 2 {
-		return n
-	}
-	dp := make([]int, n)
+	dp := make([]int, n+1)
 	dp[0] = 1
-	dp[1] = 2
-	for i := 2; i < n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= 2; j++ {
+			if i-j >= 0 {
+				dp[i] += dp[i-j]
+			}
+		}
 	}
-	return dp[n-1]
+	return dp[n]
 }
+
+// DP
+// func climbStairs(n int) int {
+// 	if n <= 2 {
+// 		return n
+// 	}
+// 	dp := make([]int, n)
+// 	dp[0] = 1
+// 	dp[1] = 2
+// 	for i := 2; i < n; i++ {
+// 		dp[i] = dp[i-1] + dp[i-2]
+// 	}
+// 	return dp[n-1]
+// }
 
 // 递归（带 memo）
 // func climbStairs(n int) int {
