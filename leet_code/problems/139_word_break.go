0849235e6a7a479b0f06894b00c59e202f1ba4fb
Wang Ben <wangben18@outlook.com>
2023-01-10 14:18:08 +0800

    139_word_break

---
+++ leet_code/problems/139_word_break.go
@@ -0,0 +1,19 @@
+package problems
+
+func wordBreak(s string, wordDict []string) bool {
+	wordMap := make(map[string]bool)
+	for _, word := range wordDict {
+		wordMap[word] = true
+	}
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && wordMap[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
