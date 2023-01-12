b1406e864a7c3dc986dd1056d0faffcdb0f179de
Wang Ben <wangben18@outlook.com>
2023-01-12 16:33:41 +0800

    121_best_time_to_buy_and_sell_stock 动态规划（滚动数组）

--- leet_code/problems/121_best_time_to_buy_and_sell_stock.go
+++ leet_code/problems/121_best_time_to_buy_and_sell_stock.go
@@ -1,18 +1,38 @@
 package problems
 
-import "math"
+// 动态规划（滚动数组）
+func maxProfit(prices []int) int {
+	dp := [2][2]int{}
+	dp[0][0] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp[i%2][0] = max(dp[(i+1)%2][0], -prices[i])
+		dp[i%2][1] = max(dp[(i+1)%2][1], prices[i]+dp[(i+1)%2][0])
+	}
+	return dp[(len(prices)+1)%2][1]
+}
+
+// 动态规划
+// func maxProfit(prices []int) int {
+// 	dp := make([][2]int, len(prices))
+// 	dp[0][0] = -prices[0]
+// 	for i := 1; i < len(prices); i++ {
+// 		dp[i][0] = max(dp[i-1][0], -prices[i])
+// 		dp[i][1] = max(dp[i-1][1], prices[i]+dp[i-1][0])
+// 	}
+// 	return dp[len(prices)-1][1]
+// }
 
 // 贪心
-func maxProfit(prices []int) int {
-	min := math.MaxInt32
-	result := 0
-	for _, price := range prices {
-		if price < min {
-			min = price
-		}
-		if price-min > result {
-			result = price - min
-		}
-	}
-	return result
-}
+// func maxProfit(prices []int) int {
+// 	min := math.MaxInt32
+// 	result := 0
+// 	for _, price := range prices {
+// 		if price < min {
+// 			min = price
+// 		}
+// 		if price-min > result {
+// 			result = price - min
+// 		}
+// 	}
+// 	return result
+// }
