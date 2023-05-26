74bff2772b6506b7d76aca0ce9eb9dec85389cf7
Wang Ben <wangben18@outlook.com>
2023-05-26 19:05:34 +0800

    1035-uncrossed-lines

---
+++ leet_code/problems/1035-uncrossed-lines_test.go
@@ -0,0 +1,39 @@
+package problems
+
+import "testing"
+
+func Test_maxUncrossedLines(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				nums1: []int{2, 5, 1, 2, 5},
+				nums2: []int{10, 5, 2, 1, 5, 2},
+			},
+			want: 3,
+		},
+		{
+			name: "",
+			args: args{
+				nums1: []int{1, 3, 7, 1, 7, 5},
+				nums2: []int{1, 9, 2, 5, 1},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxUncrossedLines(tt.args.nums1, tt.args.nums2); got != tt.want {
+				t.Errorf("maxUncrossedLines() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
