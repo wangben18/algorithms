60974e4c51119d28a45cf3fd441dda8ad15ce3ed
Wang Ben <wangben18@outlook.com>
2023-05-26 19:18:17 +0800

    53_maximum_subarray

--- leet_code/problems/53_maximum_subarray.go
+++ leet_code/problems/53_maximum_subarray.go
@@ -1,16 +1,32 @@
 package problems
 
+// 动态规划
 func maxSubArray(nums []int) int {
-	result, cur := nums[0], 0
-	for _, num := range nums {
-		cur += num
-		if cur > result {
-			result = cur
-		}
-		if cur < 0 {
-			cur = 0
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	result := dp[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(nums[i], dp[i-1]+nums[i])
+		if dp[i] > result {
+			result = dp[i]
 		}
 	}
 
 	return result
 }
+
+// 贪心
+// func maxSubArray(nums []int) int {
+// 	result, cur := nums[0], 0
+// 	for _, num := range nums {
+// 		cur += num
+// 		if cur > result {
+// 			result = cur
+// 		}
+// 		if cur < 0 {
+// 			cur = 0
+// 		}
+// 	}
+
+// 	return result
+// }
