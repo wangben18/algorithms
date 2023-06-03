735099953a256cff0929bfc566350558df9cab76
Wang Ben <wangben18@outlook.com>
2023-06-03 20:28:55 +0800

    516-ongest-palindromic-subsequence

---
+++ leet_code/problems/516-ongest-palindromic-subsequence.go
@@ -0,0 +1,19 @@
+package problems
+
+func longestPalindromeSubseq(s string) int {
+	dp := make([][]int, len(s))
+	for i := range dp {
+		dp[i] = make([]int, len(s))
+		dp[i][i] = 1
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][len(s)-1]
+}
---
+++ leet_code/problems/516-ongest-palindromic-subsequence_test.go
@@ -0,0 +1,36 @@
+package problems
+
+import "testing"
+
+func Test_longestPalindromeSubseq(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				s: "bbbab",
+			},
+			want: 4,
+		},
+		{
+			name: "",
+			args: args{
+				s: "cbbd",
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindromeSubseq(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindromeSubseq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
