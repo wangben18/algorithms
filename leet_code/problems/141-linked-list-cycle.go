856716759efe6018b29b18151ff8551ad1f361d6
Wang Ben <wangben18@outlook.com>
2023-07-20 14:44:34 +0800

    141-linked-list-cycle

---
+++ leet_code/problems/141-linked-list-cycle.go
@@ -0,0 +1,15 @@
+package problems
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
